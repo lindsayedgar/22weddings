16791a1a19818dfbbc1c35c967ab9c6712211e59
mxviteri <mxviteri@gmail.com>
2018-02-06 20:26:38 -0600

    restructuring

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -16,12 +16,9 @@ class Home extends React.Component {
         <div className="home">
           <div className="parallax"></div>
           <div className="content">
-            <span id="aboutme" className="anchor"></span>
-            <div className="about">
-              <div className="img">
-                <img src={Profile} />
-              </div>
-              <div className="text">
+            <div className="flex">
+              <span id="aboutme" className="anchor"></span>
+              <div className="about">
                 <h1>About Lindsay</h1>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
@@ -30,6 +27,27 @@ class Home extends React.Component {
                   fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
                   mollit anim id est laborum
                 </p>
+              </div>
+              <div className="services">
+                <h1>Services & Testimonials</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                  ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                  fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum
+                </p>
+              </div>
+            </div>
+            <div className="contact">
+              <h1>Contact</h1>
+              <div className="flex">
+                <div className="img">
+                  <img src={Profile} />
+                </div>
+                <div className="form">
+                  <p>Testing</p>
+                </div>
               </div>
             </div>
           </div>
