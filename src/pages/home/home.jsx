f3b244978435acd968dd446307cf793c8db6bd28
mxviteri <mxviteri@gmail.com>
2018-02-10 12:41:49 -0600

    finished content integration for homepage

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,19 +3,20 @@
 import React from 'react';
 import Header from '../shared/header/header.jsx';
 import { TextField, FlatButton } from 'material-ui';
+import http from '../../actions/http';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
 
     this.contentIds = [
-      '3Fl6RcJko0IqIaOgAec4wG',
-      '6LS6Cbj3YAoSiyAOiEGsE4',
-      '5gPtavTflS4UWIEKOSGkkk'
+      '7Cb6mNREJOQm8o8Ueys0YI',
+      '6FtU5omrJKiig20EYIuCqK',
+      '3tn9TUMPZ6SYuOAkSmWQO4'
     ];
-    // this.contentFuncs = this.contentIds.map((id) => {
-    //   return Promise.resolve(contentModule.getEntry(id));
-    // });
+    this.contentFuncs = this.contentIds.map((id) => {
+      return http.post(constants.routes.GET_CONTENT, { entryId: id });
+    });
     this.styles = {
       underlineStyle: {
         borderColor: '#F26B4D'
@@ -34,22 +35,7 @@ class Home extends React.Component {
   }
 
   componentWillMount() {
-    // this.getContent()
-    //   .then((output) => {
-    //     let about, services, testimonials;
-    //     [about, services, testimonials] = output;
-    //     const stateObj = {
-    //       aboutTitle: about.title,
-    //       aboutText: about.categoryDescription,
-    //       servicesTitle: services.title,
-    //       servicesText: services.categoryDescription,
-    //       testimonialsTitle: testimonials.title,
-    //       testimonialsText: testimonials.categoryDescription
-    //     };
-    //     this.setState({
-    //       content: stateObj
-    //     });
-    //   })
+    this.getContent();
   }
 
   render() {
@@ -116,7 +102,22 @@ class Home extends React.Component {
   }
 
   getContent = () => {
-    return Promise.all(this.contentFuncs);
+    Promise.all(this.contentFuncs)
+      .then((results) => {
+        let about, services, testimonials;
+        [about, services, testimonials] = results.map((res) => res.data.fields);
+        const stateObj = {
+          aboutTitle: about.title,
+          aboutText: about.categoryDescription,
+          servicesTitle: services.title,
+          servicesText: services.categoryDescription,
+          testimonialsTitle: testimonials.title,
+          testimonialsText: testimonials.categoryDescription
+        };
+        this.setState({
+          content: stateObj
+        });
+      });
   }
 
   submitForm = (e) => {
