dc3b25367eff5531faeea369a54e41470d4a06e8
mxviteri <mxviteri@gmail.com>
2018-02-10 10:58:27 -0600

    starting contentful integration

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -8,11 +8,48 @@ class Home extends React.Component {
   constructor(props) {
     super(props);
 
+    this.contentIds = [
+      '3Fl6RcJko0IqIaOgAec4wG',
+      '6LS6Cbj3YAoSiyAOiEGsE4',
+      '5gPtavTflS4UWIEKOSGkkk'
+    ];
+    // this.contentFuncs = this.contentIds.map((id) => {
+    //   return Promise.resolve(contentModule.getEntry(id));
+    // });
     this.styles = {
       underlineStyle: {
         borderColor: '#F26B4D'
       }
     };
+    this.state = {
+      content: {
+        aboutTitle: null,
+        aboutText: null,
+        servicesTitle: null,
+        servicesText: null,
+        testimonialsTitle: null,
+        testimonialsText: null
+      }
+    };
+  }
+
+  componentWillMount() {
+    // this.getContent()
+    //   .then((output) => {
+    //     let about, services, testimonials;
+    //     [about, services, testimonials] = output;
+    //     const stateObj = {
+    //       aboutTitle: about.title,
+    //       aboutText: about.categoryDescription,
+    //       servicesTitle: services.title,
+    //       servicesText: services.categoryDescription,
+    //       testimonialsTitle: testimonials.title,
+    //       testimonialsText: testimonials.categoryDescription
+    //     };
+    //     this.setState({
+    //       content: stateObj
+    //     });
+    //   })
   }
 
   render() {
@@ -25,37 +62,19 @@ class Home extends React.Component {
             <div className="flex">
               <span id="about" className="anchor"></span>
               <div className="about">
-                <h1>About Lindsay</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                  ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                  fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum
-                </p>
+                <h1>{this.state.content.aboutTitle}</h1>
+                <p>{this.state.content.aboutText}</p>
               </div>
               <div className="services-home">
-                <h1>Services</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                  ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                  fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum
-                </p>
+                <h1>{this.state.content.servicesTitle}</h1>
+                <p>{this.state.content.servicesText}</p>
               </div>
             </div>
             <div className="contact">
               <div className="flex">
                 <div className="testimonials">
-                  <h1>Testimonials</h1>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                    mollit anim id est laborum
-                  </p>
+                  <h1>{this.state.content.testimonialsTitle}</h1>
+                  <p>{this.state.content.testimonialsText}</p>
                 </div>
                 <div className="form">
                   <span id="contact" className="anchor"></span>
@@ -94,6 +113,10 @@ class Home extends React.Component {
         </div>
       </React.Fragment>
     )
+  }
+
+  getContent = () => {
+    return Promise.all(this.contentFuncs);
   }
 
   submitForm = (e) => {
