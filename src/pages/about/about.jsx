c99b1a0edf49ca17cbdc49b068141801d7cd44f4
mxviteri <mxviteri@gmail.com>
2018-03-06 20:44:38 -0600

    about page updates

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -31,20 +31,25 @@ class About extends React.Component {
     const about = content && content.filter((entry) => {
       return entry.sys.contentType.sys.id === 'about';
     });
+    about.sort((a, b) => {
+      return a.fields.position - b.fields.position;
+    });
 
     return (
       <React.Fragment>
         <Header />
         <div className="about-page">
+          <h1>About Twenty-Two Weddings</h1>
           {about && about.map((about, key) => {
             const url = about.fields.reference.fields.image.fields.file.url;
             return (
-              <div key={key} className="content">
-                <h1>{about.fields.title}</h1>
+              <div key={key} className="content" image-first={about.fields.imageFirst.toString()}>
                 <div className="content__image">
                   <img src={`https:${url}`} />
+                  <span>{about.fields.caption}</span>
                 </div>
                 <div className="content__text">
+                  <h3>{about.fields.title}</h3>
                   <p>{about.fields.description}</p>
                 </div>
               </div>
